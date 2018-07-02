a7b753d1cea9e828cb82c5e88aba072d98bc66bc
qweqwe159 <35982705+qweqwe159@users.noreply.github.com>
2018-07-02 09:16:34 +0900

    a

---
+++ javaStudy/src/ch23/ComparatorStinrg.java
@@ -0,0 +1,34 @@
+package ch23;
+
+import java.util.Comparator;
+import java.util.TreeSet;
+
+class ComparatorString implements Comparator<String> {
+
+	@Override
+	public int compare(String o1, String o2) {
+		return o1.length() - o2.length();
+	}
+
+}
+
+class OriginString implements Comparator<String> {
+
+	@Override
+	public int compare(String o1, String o2) {
+		return 0;
+	}
+
+}
+
+public class ComparatorStinrg {
+	public static void main(String[] args) {
+		TreeSet<String> ts = new TreeSet<>(new ComparatorString());
+		ts.add("abc");
+		ts.add("dddd");
+		ts.add("aaaadddd");
+		for (String s : ts)
+			System.out.println(s);
+
+	}
+}
