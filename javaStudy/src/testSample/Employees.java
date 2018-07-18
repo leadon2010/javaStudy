ad4d917ffe8fb1f4a9d88ea1b7129f18fc2c8bba
spring <spring@DESKTOP-D0U3BLT>
2018-07-18 09:33:46 +0900

    1

---
+++ javaStudy/src/testSample/Employees.java
@@ -0,0 +1,37 @@
+package testSample;
+
+class Employees {
+	private String firstName;
+	private String lastName;
+	private int salary;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "[Name=" + firstName + " " + lastName + ", salary=" + salary + "]";
+	}
+
+}
