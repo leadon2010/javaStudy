1b7665d43df55e2618214088303aced290cf5c18
spring <spring@DESKTOP-D0U3BLT>
2018-07-20 07:11:15 +0900

    f

---
+++ javaStudy/src/testSample/Diamond.java
@@ -0,0 +1,27 @@
+package testSample;
+
+public class Diamond {
+	public static void main(String[] main) {
+		int array[][] = new int[5][5];
+		int row, col;
+		char ch = 'A';
+		for (row = 0; row <= 4; row++) {
+			if (row <= 2) {
+				for (col = row; col <= 4 - row; col++) {
+					array[row][col] = ch++;
+				}
+			} else {
+				for (col = 4 - row; col <= row; col++) {
+					array[row][col] = ch++;
+				}
+			}
+		}
+		// 출력
+		for (row = 0; row <= 4; row++) {
+			for (col = 0; col <= 4; col++) {
+				System.out.printf("%3c", array[row][col]);
+			}
+			System.out.println();
+		}
+	}
+}
