053644c3627501bf82f8c3ef1cc3d5380e90d9c4
leadon <leadon@leadon-PC>
2018-07-20 05:21:54 +0900

    5

--- javaStudy/src/ch34/MutualAccessReentrantLock.java
+++ javaStudy/src/ch34/MutualAccessReentrantLock.java
@@ -1,5 +1,6 @@
 package ch34;
 
+import java.util.Date;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -37,18 +38,20 @@ public class MutualAccessReentrantLock {
 
 	public static void main(String[] args) throws InterruptedException {
 		Runnable task1 = () -> {
-			for (int i = 0; i < 1000; i++)
+			for (int i = 0; i < 1000000000; i++)
 				cnt.increment();
 		};
 		Runnable task2 = () -> {
-			for (int i = 0; i < 1000; i++)
+			for (int i = 0; i < 1000000000; i++)
 				cnt.decrement();
 		};
+		System.out.println(new Date());
 		ExecutorService exr = Executors.newFixedThreadPool(2);
 		exr.submit(task1);
 		exr.submit(task2);
 		exr.shutdown();
 		exr.awaitTermination(100, TimeUnit.SECONDS);
 		System.out.println(cnt.getCount());
+		System.out.println(new Date());
 	}// end of main
 }// end of class
