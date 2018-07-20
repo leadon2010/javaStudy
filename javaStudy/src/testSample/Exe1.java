c71a43295d332bdf13042d2da93eb300eb44f292
spring <spring@DESKTOP-D0U3BLT>
2018-07-20 20:23:45 +0900

    g

---
+++ javaStudy/src/testSample/Exe1.java
@@ -0,0 +1,25 @@
+package testSample;
+
+interface Printable {
+	void print(String s);
+}
+
+interface Calculable {
+	int add(int a, int b);
+}
+
+public class Exe1 {
+	public static void main(String[] args) {
+		Printable prn = (s) -> {
+			System.out.println("출력값: " + s);
+		};
+		prn.print("lch");
+
+		Calculable cl = (a, b) -> {
+			return (a + b);
+		};
+
+		System.out.println("두 수의 합은: " + cl.add(4, 5));
+
+	}// end of main
+}// end of class
