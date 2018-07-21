1e9554ebbaf33023ba93a1e2ccad75a0ec61b438
spring <spring@DESKTOP-D0U3BLT>
2018-07-21 21:32:44 +0900

    w

---
+++ javaStudy/src/ch04/PostfixOp.java
@@ -0,0 +1,23 @@
+package ch04;
+
+public class PostfixOp {
+	public static void main(String[] args) {
+		int num = 5;
+		System.out.println(--num);
+		System.out.println(--num);
+		System.out.println(num);
+
+		System.out.println(--num);
+		System.out.println(--num);
+		System.out.println(num);
+
+		System.out.println("==================");
+		System.out.println(num++);
+		System.out.println(num++);
+		System.out.println(num);
+
+		System.out.println(num--);
+		System.out.println(num--);
+		System.out.println(num);
+	}
+}
