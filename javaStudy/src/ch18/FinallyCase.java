635795176f64efb60c96a51e2b1258685cd863f2
leadon <leadon@leadon-PC>
2018-08-03 06:37:33 +0900

    dd

---
+++ javaStudy/src/ch18/FinallyCase.java
@@ -0,0 +1,30 @@
+package ch18;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FinallyCase {
+	public static void main(String[] args) {
+		Path file = Paths.get("c:\\javastudy\\simple.txt");
+		BufferedWriter writer = null;
+		try {
+			writer = Files.newBufferedWriter(file);
+			writer.write('A');
+			writer.write('Z');
+
+		} catch (IOException e) {
+			e.printStackTrace();
+
+		} finally {
+			if (writer != null)
+				try {
+					writer.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+		}
+	}// end of main
+}// end of class
---
+++ javaStudy/src/ch18/TryWithResource.java
@@ -0,0 +1,22 @@
+package ch18;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class TryWithResource {
+	public static void main(String[] args) {
+		Path file = Paths.get("c:\\javastudy\\simple.txt");
+
+		try (BufferedWriter writer = Files.newBufferedWriter(file)) {
+			writer.write('A');
+			writer.write('Z');
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
