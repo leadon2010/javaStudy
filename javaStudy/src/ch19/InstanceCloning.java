b909504582cbba7c66f1ab53156bfbe66a19bb41
leadon <leadon@leadon-PC>
2018-08-03 06:00:57 +0900

    mm

--- javaStudy/src/ch19/InstanceCloning.java
+++ javaStudy/src/ch19/InstanceCloning.java
@@ -29,6 +29,9 @@ public class InstanceCloning {
 			cpy = (Point1) org.clone();
 			org.showPosition();
 			cpy.showPosition();
+			org = new Point1(3, 4);
+			org.showPosition();
+			cpy.showPosition();
 		} catch (CloneNotSupportedException e) {
 			e.printStackTrace();
 		}
