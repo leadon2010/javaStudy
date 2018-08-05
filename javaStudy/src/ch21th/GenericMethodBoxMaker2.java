3b7216f989eb4382463865a81814bc5ef5351627
spring <spring@DESKTOP-D0U3BLT>
2018-08-06 07:18:16 +0900

    11

--- javaStudy/src/ch21th/GenericMethodBoxMaker2.java
+++ javaStudy/src/ch21th/GenericMethodBoxMaker2.java
@@ -1,14 +1,26 @@
 package ch21th;
 
+class BoxGG<T> {
+	private T ob;
+
+	public void set(T o) {
+		ob = o;
+	}
+
+	public T get() {
+		return ob;
+	}
+}
+
 class Unboxer {
-	public static <T> T openBox(BoxF<T> box) {
+	public static <T> T openBox(BoxGG<T> box) {
 		return box.get();
 	}
 }
 
 class GenericMethodBoxMaker2 {
 	public static void main(String[] args) {
-		BoxF<String> box = new BoxF<>();
+		BoxGG<String> box = new BoxGG<>();
 		box.set("My Generic Method");
 
 		String str = Unboxer.<String>openBox(box);
