25533e8f13e315ba9261849f182f2742003c1e57
leadon <leadon@DESKTOP-R5PM2VS>
2019-01-31 11:45:02 +0900

    uio

---
+++ javaStudy/src/lambda/Example3.java
@@ -0,0 +1,31 @@
+package lambda;
+
+import java.util.function.ToIntFunction;
+
+public class Example3 {
+	private static Student[] student = { new Student("홍길동", 90, 96), new Student("신용권", 95, 93) };
+
+	public static double avg(ToIntFunction<Student> function) {
+		int sum = 0;
+		double avg = 0;
+		for (Student st : student) {
+			sum += function.applyAsInt(st);
+		}
+		avg = (double) sum / student.length;
+		return avg;
+	}
+
+	public static void main(String[] args) {
+		double englishAvg = avg(s -> s.getEnglishScore());
+		System.out.println("영어 평균 점수: " + englishAvg);
+
+		englishAvg = avg(Student::getEnglishScore);
+		System.out.println("영어 평균 점수: " + englishAvg);
+
+		double mathAvg = avg(s -> s.getMathScore());
+		System.out.println("수학 평균 점수: " + mathAvg);
+		mathAvg = avg(Student::getMathScore);
+		System.out.println("수학 평균 점수: " + mathAvg);
+	}
+
+}
