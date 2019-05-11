9a7f7b4a3957b386babf53f0fbe27c09f26d51a3
leadon <leadon@DESKTOP-R5PM2VS>
2019-05-11 14:36:10 +0900

    j

---
+++ javaStudy/src/variablesAndTypes/Variable.java
@@ -0,0 +1,19 @@
+package variablesAndTypes;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		int score, numbers, total;
+		score = 10;
+		int totals = 36;
+
+		int a = 30;
+		int b = 07;
+		int c = 0x1A;
+
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+	}
+
+}
