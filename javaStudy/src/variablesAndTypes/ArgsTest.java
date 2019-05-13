6640248ce26987ba6781be760d3325491612b226
leadon <leadon@DESKTOP-R5PM2VS>
2019-05-13 12:31:15 +0900

    h

--- javaStudy/src/variablesAndTypes/ArgsTest.java
+++ javaStudy/src/variablesAndTypes/ArgsTest.java
@@ -4,11 +4,12 @@ public class ArgsTest {
 	public static void main(String[] args) {
 		System.out.println("start");
 		funs(1, 2, 3, 4, 5, 6, 7, 8);
+		System.out.println("end");
 	}
 
 	public static void funs(int... a) {
-		for (int i = 0; i < a.length; i++) {
-			System.out.println(a.toString());
+		for (int i : a) {
+			System.out.println(i);
 		}
 	}
 }
