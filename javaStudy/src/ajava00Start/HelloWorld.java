3156a8fa7d4c1925cd23b34dba1e7edcb10af201
leadon <leadon@DESKTOP-AK9J3G7>
2019-05-13 17:45:51 +0900

    a

---
+++ javaStudy/src/ajava00Start/HelloWorld.java
@@ -0,0 +1,13 @@
+package ajava00Start;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("First Java Program");
+		for (String ar : args) {
+			System.out.println(ar.toString());
+		}
+		System.out.println(7);
+		System.out.println('A');
+	}
+}
