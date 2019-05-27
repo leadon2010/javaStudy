ad79b86b94ee5b1ea416412aa1806e5da39086a9
leadon <leadon@DESKTOP-AK9J3G7>
2019-05-27 15:54:18 +0900

    f

--- javaStudy/src/method/BankAccountEx.java
+++ javaStudy/src/method/BankAccountEx.java
@@ -10,11 +10,19 @@ public class BankAccountEx {
 		bank1.withdraw(3000);
 		bank1.checkMyBalance();
 
-		bank2.deposit(5000);
-		bank2.checkMyBalance();
-		bank2.withdraw(3000);
-		bank2.checkMyBalance();
+//		bank2.deposit(5000);
+//		bank2.checkMyBalance();
+//		bank2.withdraw(3000);
+//		bank2.checkMyBalance();
 
+		try {
+			BankAccount bank3 = null;
+			System.out.println(bank3.accNumber);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("-----------------");
+		}
+		System.out.println("---------------------");
 		check(bank1);
 	}
 
