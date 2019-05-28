da38ddccce868e7a0c0ca6129a9c313e6dfcb014
leadon <leadon@DESKTOP-R5PM2VS>
2019-05-28 11:00:49 +0900

    i

--- javaStudy/src/control/Test.java
+++ javaStudy/src/control/Test.java
@@ -2,17 +2,25 @@ package control;
 
 public class Test {
 	public static void main(String[] args) {
+		gugudan(3, 5);
+	}
 
-		System.out.println("");
-		boolean bool;
-		// 6의 배수 출력(1 ~ 100)
-		// 7의 배수 출력(101 ~ 200)
-		for (int i = 1; i <= 200; i++) {
-			if (i % 6 == 0 && i <= 100) {
-				System.out.println(i);
-			} else if (i % 7 == 0 && i > 100)
-				System.out.println(i);
+	public static void gugudan(int a) {
+		System.out.println(a + "단 입니다.");
+		for (int i = 1; i <= 9; i++) {
+			System.out.println(a + " * " + i + " = " + (a * i));
 		}
+	}
 
+	public static void gugudan(int a, int b) {
+		System.out.println("구구단입니다.");
+		for (int i = 1; i <= 9; i++) {
+			for (int j = 2; j <= 9; j++) {
+				if (j == a || j == b) {
+					System.out.print(j + " * " + i + " = " + (j * i) + "\t");
+				}
+			}
+			System.out.println();
+		}
 	}
 }
