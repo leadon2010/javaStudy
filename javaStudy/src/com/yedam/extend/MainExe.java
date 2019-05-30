366a28edca02aa61648482a5bd4d9447266c9501
leadon <leadon@DESKTOP-R5PM2VS>
2019-05-30 20:58:25 +0900

    l

--- javaStudy/src/com/yedam/extend/MainExe.java
+++ javaStudy/src/com/yedam/extend/MainExe.java
@@ -4,7 +4,13 @@ public class MainExe {
 	public static void main(String[] args) {
 		String str1 = "abcdEfg";
 		String str2 = "abcdefg";
-		System.out.println(str1.compareTo(str2));
-
+		// System.out.println(str1.concat(str2));
+		StringBuilder sb = new StringBuilder();
+		sb.append("abcd");
+		sb.append("efgh");
+		sb.insert(1, "ttt");
+		sb.replace(1, 4, "yyy");
+		sb.delete(1, 4);
+		System.out.println(sb.toString());
 	}
 }
