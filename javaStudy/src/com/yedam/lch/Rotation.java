29393a3f9c11439d9204d62ff9ca0e674facfaa3
leadon <leadon@DESKTOP-R5PM2VS>
2019-05-30 18:19:06 +0900

    g

---
+++ javaStudy/src/com/yedam/lch/Rotation.java
@@ -0,0 +1,20 @@
+package com.yedam.lch;
+
+public class Rotation {
+	public static void main(String[] args) {
+		int[][] ary = new int[5][5];
+		int cnt = 0;
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				ary[i][j] = cnt++;
+			}
+		}
+
+		for (int i = 4; i >= 0; i--) {
+			for (int j = 4; j >= 0; j--) {
+				System.out.printf("%3d", ary[i][j]);
+			}
+			System.out.println();
+		}
+	}
+}
