2cce302403ffabe797ad96aa85c3dc4f8e675aba
leadon <leadon@DESKTOP-R5PM2VS>
2019-06-01 09:04:22 +0900

    jkl

--- javaStudy/src/com/yedam/extend/MainExe.java
+++ javaStudy/src/com/yedam/extend/MainExe.java
@@ -2,13 +2,13 @@ package com.yedam.extend;
 
 public class MainExe {
 	public static void main(String[] args) {
-		Parent parent = new Parent();
+		Parent parent = new ParentChild();
 		parent.field1 = "data1";
 		parent.method1();
 		parent.method2();
-		
+
 		ParentChild child = (ParentChild) parent;
-		
+
 		child.field2 = "data2";
 		child.method3();
 	}
