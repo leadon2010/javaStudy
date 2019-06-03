f04ff5073b22dc9bf91a9e1214f68851d579a915
leadon <leadon@DESKTOP-AK9J3G7>
2019-06-04 04:56:18 +0900

    f

--- javaStudy/src/com/yedam/abstractPkg/PhoneExample.java
+++ javaStudy/src/com/yedam/abstractPkg/PhoneExample.java
@@ -3,9 +3,10 @@ package com.yedam.abstractPkg;
 public class PhoneExample {
 	public static void main(String[] args) {
 		// Phone phone = new Phone();
-		SmartPhone sp = new SmartPhone("nice");
-		sp.turnOn();
-		sp.internetSearch();
-		sp.turnOff();
+		Phone phone = new SmartPhone("nice"); // 자동형변환
+		phone.turnOn();
+		SmartPhone smartPhone = (SmartPhone) phone; // 강제형변환
+		smartPhone.internetSearch();
+		phone.turnOff();
 	}
 }
