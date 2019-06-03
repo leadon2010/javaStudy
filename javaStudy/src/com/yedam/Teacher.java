40f8fcc860f4a7323fdfefef9efb29220fd9f1e1
leadon <leadon@DESKTOP-AK9J3G7>
2019-06-03 23:52:37 +0900

    d

---
+++ javaStudy/src/com/yedam/Teacher.java
@@ -0,0 +1,24 @@
+package com.yedam;
+
+import java.util.Formatter;
+
+public class Teacher extends People {
+	private String studyClass;
+
+	public Teacher(String name, String ssn) {
+		super(name, ssn);
+	}
+
+	public Teacher(String name, String ssn, String studyClass) {
+		super(name, ssn);
+		this.studyClass = studyClass;
+	}
+
+	@Override
+	public String toString() {
+		Formatter fm = new Formatter();
+		fm.format("name= %s, ssn= %s, studyClass= %s", name, ssn, studyClass);
+		return fm.toString();
+	}
+
+}
--- javaStudy/src/zone/study/ImportSubject.java
+++ javaStudy/src/zone/study/ImportSubject.java
@@ -1,13 +1,57 @@
 package zone.study;
 
-public class ImportSubject {
-	public static void main(String[] args) {
-		System.out.println("program start");
-		println();
-		System.out.println("program end");
+import com.yedam.Student;
+import com.yedam.Teacher;
+
+class StudyClass {
+	Teacher teacher;
+	Student[] sAry;
+
+	public Teacher getTeacher() {
+		return teacher;
 	}
 
-	static void println() {
-		System.out.println("======================");
+	public void setTeacher(Teacher teacher) {
+		this.teacher = teacher;
+	}
+
+	public Student[] getsAry() {
+		return sAry;
+	}
+
+	public void setsAry(Student[] sAry) {
+		this.sAry = sAry;
+	}
+
+}
+
+public class ImportSubject {
+	public static void main(String[] args) {
+		StudyClass sclass1 = new StudyClass();
+		
+		Teacher teacher = new Teacher("teacher1", "t1111", "english");
+
+		Student[] sAry = new Student[3];
+		sclass1.setTeacher(teacher);
+		sAry[0] = new Student("sname1", "s1111", "english");
+		sAry[1] = new Student("sname2", "s2222", "english");
+		sAry[2] = new Student("sname3", "s3333", "english");
+		sclass1.setsAry(sAry);
+		
+		showClassList(sclass1);
+	}
+
+	static void showClassList(StudyClass sc) {
+		System.out.println("Teacher name: " + sc.getTeacher().getName());
+		System.out.println("Teacher no: " + sc.getTeacher().getSsn());
+		for (int i = 0; i < sc.getsAry().length; i++) {
+			System.out.println("sname: " + sc.getsAry()[i].getName() + ", sno: " + sc.getsAry()[i].getSsn() + ", major: " + sc.getsAry()[i].getMajor());
+		}
+
+	}
+
+	static void println(int a, String str) {
+		for (int i = 0; i < a; i++)
+			System.out.println(str);
 	}
 }
