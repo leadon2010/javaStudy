cceaf97f88fc1b2be3cbd5722f9becee6a9da0a8
leadon <leadon@DESKTOP-R5PM2VS>
2019-06-03 11:59:04 +0900

    ,

--- javaStudy/src/com/yedam/MainExe.java
+++ javaStudy/src/com/yedam/MainExe.java
@@ -2,8 +2,6 @@ package com.yedam;
 
 public class MainExe {
 	public static void main(String[] args) {
-		Student st = new Student("name1", "12345", "english");
-		System.out.println(st.getName() + " " + st.getSsn() + " " + st.getMajor());
-		// System.out.println(st.toString());
+
 	}
 }
