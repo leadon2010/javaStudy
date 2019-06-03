31b0a838688c0412edfef72b1ad4e9d6e105d49d
leadon <leadon@DESKTOP-AK9J3G7>
2019-06-04 04:25:04 +0900

    f

--- javaStudy/src/zone/study/ImportSubject.java
+++ javaStudy/src/zone/study/ImportSubject.java
@@ -27,8 +27,9 @@ class StudyClass {
 
 public class ImportSubject {
 	public static void main(String[] args) {
-		StudyClass sclass1 = new StudyClass();
 		
+		StudyClass sclass1 = new StudyClass();
+
 		Teacher teacher = new Teacher("teacher1", "t1111", "english");
 
 		Student[] sAry = new Student[3];
@@ -37,15 +38,18 @@ public class ImportSubject {
 		sAry[1] = new Student("sname2", "s2222", "english");
 		sAry[2] = new Student("sname3", "s3333", "english");
 		sclass1.setsAry(sAry);
-		
+
 		showClassList(sclass1);
 	}
 
 	static void showClassList(StudyClass sc) {
+		System.out.println("[교수정보]");
 		System.out.println("Teacher name: " + sc.getTeacher().getName());
 		System.out.println("Teacher no: " + sc.getTeacher().getSsn());
+		System.out.println("[학생정보]");
 		for (int i = 0; i < sc.getsAry().length; i++) {
-			System.out.println("sname: " + sc.getsAry()[i].getName() + ", sno: " + sc.getsAry()[i].getSsn() + ", major: " + sc.getsAry()[i].getMajor());
+			System.out.println("sname: " + sc.getsAry()[i].getName() + ", sno: " + sc.getsAry()[i].getSsn()
+					+ ", major: " + sc.getsAry()[i].getMajor());
 		}
 
 	}
