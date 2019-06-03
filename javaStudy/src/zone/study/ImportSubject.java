1b212bb67856dadf6ba16a5541d3de9986fae16e
leadon <leadon@DESKTOP-R5PM2VS>
2019-06-03 18:02:23 +0900

    k

---
+++ javaStudy/src/zone/study/ImportSubject.java
@@ -0,0 +1,13 @@
+package zone.study;
+
+public class ImportSubject {
+	public static void main(String[] args) {
+		System.out.println("program start");
+		println();
+		System.out.println("program end");
+	}
+
+	static void println() {
+		System.out.println("======================");
+	}
+}
