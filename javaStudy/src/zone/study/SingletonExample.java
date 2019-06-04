952de839dc9ec0261910b2e4e70caab8502993e8
leadon <leadon@DESKTOP-AK9J3G7>
2019-06-04 20:32:55 +0900

    ㅇ

--- javaStudy/src/zone/study/SingletonExample.java
+++ javaStudy/src/zone/study/SingletonExample.java
@@ -27,6 +27,38 @@ public class SingletonExample {
 		else
 			System.out.println("다른 Singleton 입니다.");
 
+		// main메소드 호출 시 넘겨주는 인자값 확인하기.
+		String[] strAry = { "str1", "str2", "str3" };
+		PersonExample.main(strAry);
 	}
+}
 
+//final 필드와 상수.
+//상수 = static final 의 의미가 있다.
+class Person {
+	final String nation = "Korea";
+	final String ssn;
+	String name;
+
+	public Person(String ssn, String name) {
+		this.ssn = ssn;
+		this.name = name;
+	}
+}
+
+class PersonExample {
+	public static void main(String[] args) {
+		// main 메소드 호출 시 넘겨지는 인자값 확인하기.
+		for (int i = 0; i < args.length; i++) {
+			System.out.println(args[i]);
+		}
+
+		Person person = new Person("123456789", "박문수");
+		System.out.println(person.nation);
+		System.out.println(person.ssn);
+		System.out.println(person.name);
+		// person.nation = "USA";
+		// person.ssn = "1111111";
+		person.name = "홍길동";
+	}
 }
