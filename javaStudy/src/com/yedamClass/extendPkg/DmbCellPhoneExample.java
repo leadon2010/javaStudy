ec2b5e8bc4ca11dad724840d3fd1f689be53e728
leadon <leadon@DESKTOP-R5PM2VS>
2019-06-06 14:08:42 +0900

    a

--- javaStudy/src/com/yedamClass/extendPkg/DmbCellPhoneExample.java
+++ javaStudy/src/com/yedamClass/extendPkg/DmbCellPhoneExample.java
@@ -1,8 +1,17 @@
 package com.yedamClass.extendPkg;
 
+// 부모클래스의 기능을 활용할 수 있다.
 class CellPhone {
 	String model;
 	String color;
+
+	public CellPhone() {
+		super();
+	}
+
+	CellPhone(String model) {
+		this.model = model;
+	}
 
 	void powerOn() {
 		System.out.println("전원을 켭니다.");
@@ -27,12 +36,25 @@ class CellPhone {
 	void hangUp() {
 		System.out.println("전화를 끊습니다.");
 	}
+
+	private void folder() {
+		System.out.println("폴더기능");
+	}
+
+	protected void antenna() {
+		System.out.println("안테나 기능");
+	}
 }
 
+// 자식클래스는 부모클래스 + 자신의 고유한 기능을 가진다.
 class DmbCellPhone extends CellPhone {
 	int channel;
 
+	DmbCellPhone() {
+	};
+
 	DmbCellPhone(String model, String color, int channel) {
+		super();
 		this.model = model;
 		this.color = color;
 		this.channel = channel;
@@ -54,6 +76,7 @@ class DmbCellPhone extends CellPhone {
 
 public class DmbCellPhoneExample {
 	public static void main(String[] args) {
+
 		DmbCellPhone dphone = new DmbCellPhone("김자바", "검정", 10);
 
 		// CellPhone으로 부터 상속받은 필드
@@ -70,6 +93,8 @@ public class DmbCellPhoneExample {
 		dphone.receiveVoices("안녕하세요. 저는 홍길동인데요");
 		dphone.sendVoices("반갑습니다.");
 		dphone.hangUp();
+		// dphone.folder(); // 상속을 받을 수 없다.
+		dphone.antenna();
 
 		// DmbCellPhone 의 메소드 호출
 		dphone.turnOnDmb();
@@ -80,5 +105,7 @@ public class DmbCellPhoneExample {
 		 * 예제1) Vehicle 클래스와 상속받는 Bus 클래스를 생성. 필드: engine, handle, tire + busNo,
 		 * busCompany 메소드: start(), run(), stop() + checkCard(), bell()
 		 */
+		
+		// 메소드 재정의 (@Override) & 부모메소드 호출 ==> SuperSonicAirplane.java 
 	}
 }
---
+++ javaStudy/src/com/yedamClass/extendPkg/SuperSonicAirplaneExample.java
@@ -0,0 +1,46 @@
+package com.yedamClass.extendPkg;
+
+class Airplane {
+	public void land() {
+		System.out.println("착륙합니다.");
+	}
+
+	public void fly() {
+		System.out.println("일반비행을 합니다.");
+	}
+
+	public void takeOff() {
+		System.out.println("이륙합니다.");
+	}
+}
+
+class SuperSonicAirplane extends Airplane {
+	public static final int NORMAL = 1;
+	public static final int SUPERSONIC = 2;
+
+	public int flyMode = NORMAL;
+
+	@Override
+	public void fly() {
+		if (flyMode == SUPERSONIC) {
+			System.out.println("초음속비행합니다.");
+		} else {
+			// Airplane 객체의 fly() 메소드 호출.
+			super.fly();
+		}
+
+	}
+}
+
+public class SuperSonicAirplaneExample {
+	public static void main(String[] args) {
+		SuperSonicAirplane sa = new SuperSonicAirplane();
+		sa.takeOff();
+		sa.fly();
+		sa.flyMode = SuperSonicAirplane.SUPERSONIC;
+		sa.fly();
+		sa.flyMode = SuperSonicAirplane.NORMAL;
+		sa.fly();
+		sa.land();
+	}
+}
