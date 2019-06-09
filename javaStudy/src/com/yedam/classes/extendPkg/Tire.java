fd006b4adb8520d19279fb3f750878e39c73ec03
leadon <leadon@DESKTOP-R5PM2VS>
2019-06-10 05:59:56 +0900

    ㅏ

--- javaStudy/src/com/yedam/classes/extendPkg/Tire.java
+++ javaStudy/src/com/yedam/classes/extendPkg/Tire.java
@@ -2,9 +2,9 @@ package com.yedam.classes.extendPkg;
 
 public class Tire {
 
-	public int maxRotation;
-	public int accumulateRotation;
-	public String location;
+	public int maxRotation;  // 사용가능한 회전수.
+	public int accumulateRotation; // 현재 사용한 회전수.
+	public String location; // 장착위치...왼쪽,오른쪽,앞,뒤
 
 	public Tire(String location, int maxRotation) {
 		this.location = location;
