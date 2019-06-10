b4b110a3ae95fdb71d585d9207734c4a6a05cd67
leadon <leadon@DESKTOP-R5PM2VS>
2019-06-10 20:07:52 +0900

    f

---
+++ javaStudy/src/exceptions/ClassCastExceptionExample.java
@@ -0,0 +1,31 @@
+package exceptions;
+
+class Animal {
+
+}
+
+class Dog extends Animal {
+
+}
+
+class Cat extends Animal {
+
+}
+
+public class ClassCastExceptionExample {
+	public static void main(String[] args) {
+		Dog dog = new Dog();
+		changeDog(dog);
+
+		Cat cat = new Cat();
+		changeDog(cat);
+	}
+
+	public static void changeDog(Animal animal) {
+		if (animal instanceof Dog) {
+			Dog dog = (Dog) animal;
+		} else {
+			System.out.println("변환할 수 없습니다.");
+		}
+	}
+}
