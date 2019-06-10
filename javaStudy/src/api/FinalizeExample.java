fbce29ced74c7705019f8dc950668b9bc9eec02c
leadon <leadon@DESKTOP-R5PM2VS>
2019-06-11 08:52:38 +0900

    r

---
+++ javaStudy/src/api/FinalizeExample.java
@@ -0,0 +1,27 @@
+package api;
+
+class Counter {
+	private int no;
+
+	public Counter(int no) {
+		this.no = no;
+	}
+
+	@Override
+	protected void finalize() throws Throwable {
+//		super.finalize();
+		System.out.println(no + "번 객체의 finalize()가 실행됨.");
+	}
+
+}
+
+public class FinalizeExample {
+	public static void main(String[] args) {
+		Counter counter = null;
+		for (int i = 1; i <= 50; i++) {
+			counter = new Counter(i);
+			counter = null;
+			System.gc();
+		}
+	}
+}
--- javaStudy/src/exceptions/TryWithResourceExample.java
+++ javaStudy/src/exceptions/TryWithResourceExample.java
@@ -6,6 +6,25 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+class FileInputStream implements AutoCloseable {
+	private String file;
+
+	public FileInputStream(String file) {
+		super();
+		this.file = file;
+	}
+
+	public void read() {
+		System.out.println(file + " 을 읽습니다");
+	}
+
+	@Override
+	public void close() throws Exception {
+		System.out.println(file + " 을 닫습니다");
+	}
+
+}
+
 public class TryWithResourceExample {
 	public static void main(String[] args) {
 		// try..catch..finally..
