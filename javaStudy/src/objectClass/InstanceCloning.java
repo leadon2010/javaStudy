a0479294b5c7089bbe6696c9ccbef42edd2c8748
leadon <leadon@DESKTOP-R5PM2VS>
2019-06-10 23:21:25 +0900

    f

---
+++ javaStudy/src/objectClass/InstanceCloning.java
@@ -0,0 +1,38 @@
+package objectClass;
+
+class Points implements Cloneable {
+	private int xPos;
+	private int yPos;
+
+	public Points(int x, int y) {
+		xPos = x;
+		yPos = y;
+	}
+
+	public void showPosition() {
+		System.out.printf("[%d, %d]", xPos, yPos);
+		System.out.println();
+	}
+
+	@Override
+	public Object clone() throws CloneNotSupportedException {
+		return super.clone();
+	}
+
+}
+
+public class InstanceCloning {
+	public static void main(String[] args) {
+		Points org = new Points(3, 5);
+		Points cpy;
+
+		try {
+			cpy = (Points) org.clone();
+			org.showPosition();
+			cpy.showPosition();
+			System.out.println(org + "/" + cpy);
+		} catch (CloneNotSupportedException e) {
+			e.printStackTrace();
+		}
+	}
+}
