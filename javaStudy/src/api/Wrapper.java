d0c5bd8c8b62e0fe417ab7228c0fcc980b00c718
leadon <leadon@DESKTOP-AK9J3G7>
2019-06-12 07:00:52 +0900

    h

---
+++ javaStudy/src/api/Wrapper.java
@@ -0,0 +1,14 @@
+package api;
+
+public class Wrapper {
+	public static void main(String[] args) {
+		Integer iObj = new Integer(10);
+		iObj += 20;
+		System.out.println(iObj);
+		Double dObj = new Double(3.45);
+		dObj += 2.2;
+		System.out.println(dObj);
+		dObj = new Double("3.3342343245324");
+		System.out.println(dObj);
+	}
+}
