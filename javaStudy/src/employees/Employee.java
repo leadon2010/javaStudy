6f8ff631ccb16c1fc81fb7856f65963de869599d
leadon <leadon@DESKTOP-AK9J3G7>
2019-06-13 06:39:55 +0900

    f

--- javaStudy/src/employees/Employee.java
+++ javaStudy/src/employees/Employee.java
@@ -1,4 +1,4 @@
-package arrayList;
+package employees;
 
 public class Employee {
 	private int employeeId;
--- javaStudy/src/employees/EmployeeProc.java
+++ javaStudy/src/employees/EmployeeProc.java
@@ -1,4 +1,4 @@
-package arrayList;
+package employees;
 
 import java.util.ArrayList;
 import java.util.List;
