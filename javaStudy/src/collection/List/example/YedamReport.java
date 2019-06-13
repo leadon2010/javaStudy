c54c95eb5737229676dcb28911ec7b73b2110fda
leadon <leadon@DESKTOP-AK9J3G7>
2019-06-14 05:04:45 +0900

    g

--- javaStudy/src/collection/List/example/YedamReport.java
+++ javaStudy/src/collection/List/example/YedamReport.java
@@ -1,137 +1,137 @@
 package collection.List.example;
 
 import java.util.LinkedList;
+
 import java.util.List;
+
 import java.util.Scanner;
 
+//	학생정보를 저장하는 프로그램(Ver.1.0)
+
+//	만든이: 김화정(2019.06.13)
+
+//	최종수정일: 2019.06.13
+
 class Student {
-	private String sNo = "";
-	private String name = "";
-	private String major = "";
 
-	public Student() {
+	private String sno;
+
+	private String name;
+
+	private String major;
+
+	public Student(String sn, String n, String m) {
+
+		sno = sn;
+
+		name = n;
+
+		major = m;
 
 	}
 
-	public Student(String sNo, String name, String major) {
-		// TODO Auto-generated constructor stub
-		this.sNo = sNo;
-		this.name = name;
-		this.major = major;
+	public String getSno() {
+
+		return sno;
+
 	}
 
-	public String getsNo() {
-		return sNo;
+	public void print() {
+
+		System.out.println("힉생번호:" + sno + "    이름:" + name + "    학과: " + major);
+
 	}
 
-	public String getName() {
-		return name;
-	}
-
-	public String getMajor() {
-		return major;
-	}
 }
 
 public class YedamReport {
-	static List<Student> list = new LinkedList<>();
+
+	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		boolean start = true;
-		while (start) {
-			System.out.println("1번 : 학생 저장 | 2번 : 학생 조회  | 3번 : 학생 삭제 | 4번 : 종료");
-			System.out.print(">");
-			String no = "";
-			String name = "";
-			String major = "";
-			int Number = sc.nextInt();
-			switch (Number) {
-			case 1: // 저장
-				System.out.print("학번을 입력 하세요> ");
-				no = sc.next();
-				System.out.print("이름을 입력 하세요> ");
-				name = sc.next();
-				System.out.print("전공을 입력 하세요> ");
-				major = sc.next();
-				Student student = new Student(no, name, major);
-				addUser(student, no);
-				// list.add(student);
-				System.out.println("----------------------------------------------------");
-				break;
-			case 2: // 조회
-				System.out.print("조회 하실 학생번호를 입력하세요> ");
-				no = sc.next();
-				inquire(no);
-				System.out.println("----------------------------------------------------");
-				break;
-			case 3: // 삭제
-				System.out.println("삭제하실 학생번호를 입력하세요> ");
-				no = sc.next();
-				delete(no);
-				System.out.println("----------------------------------------------------");
-				break;
-			case 4: // 탈출
-				System.out.println("프로그램을 종료합니다.");
-				start = false;
-				break;
-			default:
-				System.out.println("1~4번까지의 숫자만 넣으세요.");
-				break;
-			}
-		}
-	}
 
-	private static void addUser(Student student, String no) {
-		// System.out.println("addUser 함수 호출 성공, List Size = " + list.size());
-		for (int i = 0; i <= list.size(); i++) {
-			// System.out.println("< i = " + i + " >");
-			if (list.isEmpty()) { // list에 학생 정보가 하나도 없을 경우 무조건 추가
-				// System.out.println("< if문 >");
-				list.add(student);
-				System.out.println("'" + student.getsNo() + "'" + "번의 학생이 저장되었습니다. \n");
-				break;
-			} else if (list.get(i).getsNo().equals(no)) { // 추가하려는 학번이 기존 학생의 학번과 동일 시 추가 불가
-				// System.out.println("< else if문 >");
-				System.out.println("'" + list.get(i).getsNo() + "'" + "번 학생은 이미 존재합니다. \n");
-				break;
-			} else {
-				// System.out.println("< else문 >");
-				list.add(student);
-				System.out.println("'" + student.getsNo() + "'" + "번 학생이 저장되었습니다. \n");
-				break;
-			}
-		}
-	}
+		List<Student> list = new LinkedList<>();
 
-	private static void delete(String no) {
-		// TODO Auto-generated method stub
-		for (int i = 0; i < list.size(); i++) {
-			if (list.get(i).getsNo().equals(no)) {
-				// System.out.println("<if 문 >");
-				System.out.println("'" + list.get(i).getsNo() + "'" + "번 학생이 삭제되었습니다. \n");
-				list.remove(i);
-			} else if (list.isEmpty()) {
-				// System.out.println("< else if문 >");
-				System.out.println("'" + list.get(i).getsNo() + "'" + "번 학생은 존재하지 않습니다. \n");
-			}
-		}
-	}
+		int num = 0;
 
-	private static void inquire(String no) {
-		for (int i = 0; i < list.size(); i++) {
-			// System.out.println("i = " + i + ", List Size = " + list.size());
-			if (list.get(i).getsNo().equals(no)) {
-				System.out.println("< " + list.get(i).getsNo() + "학번의 학생 정보 >");
-				System.out.println("이름 : " + list.get(i).getName() + " | 전공 : " + list.get(i).getMajor());
+		while (num != 4) {
+
+			System.out.println("1.저장	2.조회(sno)	3.삭제(sno)	4.종료");
+
+			num = sc.nextInt();
+
+			switch (num) {
+
+			case 1:
+
+				System.out.print("학생번호:");
+
+				String sno = sc.next();
+
+				System.out.print("이름:");
+
+				String name = sc.next();
+
+				System.out.print("학과:");
+
+				String major = sc.next();
+
+				list.add(new Student(sno, name, major));
 
 				break;
-			} else {
-				if ((i + 1) == list.size()) {
-					System.out.println("'" + no + "'" + "번 학번을 가진 학생은 존재하지 않습니다.");
+
+			case 2:
+
+				System.out.println("조회할 학생번호 입력:");
+
+				String snum1 = sc.next();
+
+				for (Student s : list) {
+
+					if (s.getSno().equals(snum1)) { // list의 Student인스턴스들 중 sno가 snum1과 같은사람이 있으면
+
+						System.out.println("*****학생정보*****");
+
+						s.print(); // 출력해라
+
+						System.out.println("***************");
+
+					}
+
 				}
+
+				break;
+
+			case 3:
+
+				System.out.print("삭제할 학생번호 입력:");
+
+				String snum2 = sc.next();
+
+				for (int i = 0; i < list.size(); i++) {
+
+					if (list.get(i).getSno().equals(snum2)) { // sno가 snum2랑 같은 사람이 있으면 지워라
+
+						list.remove(i);
+
+						System.out.println("학생이 삭제되었습니다.");
+
+					}
+
+				}
+
+				break;
+
+			case 4:
+
+				System.out.println("종료합니다.");
+
+				break;
+
 			}
+
 		}
+
 	}
+
 }
