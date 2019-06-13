63e73e4ffd07ecd9b1423c60c32e2feb48c6167f
leadon <leadon@DESKTOP-AK9J3G7>
2019-06-13 17:52:30 +0900

    d

---
+++ javaStudy/src/collection/Map/TreeMapExample.java
@@ -0,0 +1,16 @@
+package collection.Map;
+
+import java.util.TreeMap;
+
+public class TreeMapExample {
+	public static void main(String[] args) {
+		TreeMap<Integer, String> scores = new TreeMap<Integer, String>();
+		scores.put(new Integer(87), "홍길동");
+		scores.put(new Integer(98), "이동수");
+		scores.put(new Integer(75), "박길순");
+		scores.put(new Integer(95), "신용권");
+		scores.put(new Integer(80), "김자바");
+		
+		
+	}
+}
