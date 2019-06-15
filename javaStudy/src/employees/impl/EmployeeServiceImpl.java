ac528a989c094d7c74c638c1f8de1a92c9e34086
leadon <leadon@DESKTOP-R5PM2VS>
2019-06-15 22:45:57 +0900

    f

---
+++ javaStudy/src/employees/impl/EmployeeServiceImpl.java
@@ -0,0 +1,35 @@
+package employees.impl;
+
+import java.util.List;
+
+import employees.EmpService;
+import employees.Employees;
+
+public class EmployeeServiceImpl implements EmpService {
+
+	@Override
+	public Employees searchEmp(int empId) {
+		return null;
+	}
+
+	@Override
+	public List<Employees> searchAll() {
+		return null;
+	}
+
+	@Override
+	public void insertEmp(Employees emp) {
+
+	}
+
+	@Override
+	public void updateEmp(Employees emp) {
+
+	}
+
+	@Override
+	public void deleteEmp(int empId) {
+
+	}
+
+}
