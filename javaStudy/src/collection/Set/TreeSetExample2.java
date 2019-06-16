386f9d80eb6b422bacd4a964e63d565a6a354f6f
leadon <leadon@DESKTOP-R5PM2VS>
2019-06-16 18:00:39 +0900

    jkl

--- javaStudy/src/collection/Set/TreeSetExample2.java
+++ javaStudy/src/collection/Set/TreeSetExample2.java
@@ -13,18 +13,28 @@ public class TreeSetExample2 {
 		scores.add(new Integer(95));
 		scores.add(new Integer(80));
 
+		System.out.println("[오름차순 iterator() ]");
 		Iterator<Integer> iter = scores.iterator();
 		while (iter.hasNext()) {
 			System.out.print(iter.next() + " ");
 		}
 		System.out.println();
 
+		System.out.println("[내림차순 iterator() ]");
+		iter = scores.descendingIterator();
+		while (iter.hasNext()) {
+			System.out.print(iter.next() + " ");
+		}
+		System.out.println();
+
+		System.out.println("[내림차순 descendingSet() ]");
 		NavigableSet<Integer> descendingSet = scores.descendingSet();
 		for (Integer score : descendingSet) {
 			System.out.print(score + " ");
 		}
 		System.out.println();
 
+		System.out.println("[오름차순 descendingSet().descendingSet()]");
 		NavigableSet<Integer> ascendingSet = scores.descendingSet().descendingSet();
 		for (Integer score : ascendingSet) {
 			System.out.print(score + " ");
