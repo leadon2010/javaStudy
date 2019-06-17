259f61ad4f19618634d70601feb23fd556b7c827
leadon <leadon@DESKTOP-R5PM2VS>
2019-06-17 17:59:46 +0900

    t

--- javaStudy/src/api/DecimalFormatExample.java
+++ javaStudy/src/api/DecimalFormatExample.java
@@ -4,8 +4,8 @@ import java.text.DecimalFormat;
 
 public class DecimalFormatExample {
 	public static void main(String[] args) {
-		double num = 1234567.89;
-		DecimalFormat df = new DecimalFormat("#,###.000");
+		double num = -1234567.89;
+		DecimalFormat df = new DecimalFormat("+#,###.##; -#,###.##");
 		System.out.println(df.format(num));
 	}
 }
---
+++ javaStudy/src/api/SimpleDateFormatExample.java
@@ -0,0 +1,11 @@
+package api;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class SimpleDateFormatExample {
+	public static void main(String[] args) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 MM월 dd일");
+		System.out.println(sdf.format(new Date()));
+	}
+}
