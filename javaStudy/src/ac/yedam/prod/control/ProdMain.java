7f222f719f2ca3cef139a11f9c35c4cab0b38960
leadon <leadon@DESKTOP-AK9J3G7>
2019-06-18 21:58:57 +0900

    f

--- javaStudy/src/ac/yedam/prod/control/ProdMain.java
+++ javaStudy/src/ac/yedam/prod/control/ProdMain.java
@@ -1,5 +1,7 @@
 package ac.yedam.prod.control;
 
 public class ProdMain {
-
+	public static void main(String[] args) {
+		
+	}
 }
---
+++ javaStudy/src/ac/yedam/prod/control/ProdProc.java
@@ -0,0 +1,20 @@
+package ac.yedam.prod.control;
+
+public class ProdProc {
+
+	// 1)상품정보 2)재고정보
+	public void execute() {
+
+	}
+
+	// 1)상품조회 2)상품등록 3)상품변경 4)상품삭제 5)상위메뉴
+	public void prodInfo() {
+
+	}
+
+	// 1)재고조회 2)입고처리 3)출고처리 4)상위메뉴
+	public void onhandInfo() {
+
+	}
+
+}
