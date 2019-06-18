4bcca48495f900cae06845e9e1e694f51f1261b2
leadon <leadon@DESKTOP-R5PM2VS>
2019-06-18 17:41:30 +0900

    j

--- javaStudy/src/com/yedam/lambda/PredicateExample.java
+++ javaStudy/src/com/yedam/lambda/PredicateExample.java
@@ -4,34 +4,34 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 
-class Student2 {
-	private String name;
-	private String sex;
-	private int score;
-
-	public Student2(String name, String sex, int score) {
-		this.name = name;
-		this.sex = sex;
-		this.score = score;
-	}
-
-	public String getSex() {
-		return sex;
-	}
-
-	public int getScore() {
-		return score;
-	}
-
-}
-
 public class PredicateExample {
-	private static List<Student2> list = Arrays.asList(new Student2("홍길동", "남자", 90), new Student2("김순희", "여자", 90),
-			new Student2("감자바", "남자", 95), new Student2("박한나", "여자", 92));
+	static class Student {
+		private String name;
+		private String sex;
+		private int score;
 
-	public static double avg(Predicate<Student2> predicate) {
+		public Student(String name, String sex, int score) {
+			this.name = name;
+			this.sex = sex;
+			this.score = score;
+		}
+
+		public String getSex() {
+			return sex;
+		}
+
+		public int getScore() {
+			return score;
+		}
+
+	}
+
+	private static List<Student> list = Arrays.asList(new Student("홍길동", "남자", 90), new Student("김순희", "여자", 90),
+			new Student("감자바", "남자", 95), new Student("박한나", "여자", 92));
+
+	public static double avg(Predicate<Student> predicate) {
 		int count = 0, sum = 0;
-		for (Student2 student : list) {
+		for (Student student : list) {
 			if (predicate.test(student)) {
 				count++;
 				sum += student.getScore();
