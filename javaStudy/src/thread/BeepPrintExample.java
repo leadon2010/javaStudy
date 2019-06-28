04ca063dff71a2eba6a760642c400180327eaaad
leadon <leadon@DESKTOP-R5PM2VS>
2019-06-29 08:13:19 +0900

    d

---
+++ javaStudy/src/thread/BeepPrintExample.java
@@ -0,0 +1,50 @@
+package thread;
+
+import java.awt.Toolkit;
+
+class BeepTask implements Runnable {
+
+	@Override
+	public void run() {
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
+		for (int i = 0; i < 5; i++) {
+			toolkit.beep();
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
+
+public class BeepPrintExample {
+	public static void main(String[] args) {
+
+//		Runnable beepTask = new BeepTask();
+		Thread thread = new Thread(() -> {
+
+			Toolkit toolkit = Toolkit.getDefaultToolkit();
+			for (int i = 0; i < 5; i++) {
+				toolkit.beep();
+				try {
+					Thread.sleep(500);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+		thread.start();
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println("print");
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
