91608fbdb1715bc99c3eafcf9ac7c6f86c0cf771
leadon <leadon@DESKTOP-R5PM2VS>
2019-06-29 15:46:09 +0900

    jkl

---
+++ javaStudy/src/javaFx/controller/ButtonController.java
@@ -0,0 +1,15 @@
+package javaFx.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class ButtonController implements Initializable {
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+	}
+
+}
