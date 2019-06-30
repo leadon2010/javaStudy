6dd7bdd54f7a44dd2de8d816778384aa87bd51c1
leadon <leadon@DESKTOP-R5PM2VS>
2019-06-30 10:24:20 +0900

    d

--- javaStudy/src/javaFx/TableViewMain.java
+++ javaStudy/src/javaFx/TableViewMain.java
@@ -4,15 +4,23 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
@@ -43,40 +51,15 @@ class Phone {
 
 }
 
-class TableViewController implements Initializable {
-	@FXML
-	private ListView<String> listView;
-	@FXML
-	private TableView<Phone> tableView;
-	@FXML
-	private ImageView imageView;
-
-	@Override
-	public void initialize(URL arg0, ResourceBundle arg1) {
-		listView.setItems(
-				FXCollections.observableArrayList("갤럭시S1", "갤럭시S2", "갤럭시S3", "갤럭시S4", "갤럭시S5", "갤럭시S6", "갤럭시S7"));
-		listView.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
-
-			@Override
-			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-				tableView.getSelectionModel().select(newValue.intValue());
-				tableView.scrollTo(newValue.intValue());
-			}
-		});
-
-		ObservableList phoneList = FXCollections.observableArrayList(new Phone("갤럭시S1", "phone01.png"),
-				new Phone("갤럭시S2", "phone02.png"), new Phone("갤럭시S3", "phone03.png"), new Phone("갤럭시S4", "phone04.png"),
-				new Phone("갤럭시S5", "phone05.png"), new Phone("갤럭시S6", "phone06.png"),
-				new Phone("갤럭시S7", "phone07.png"));
-	}
-
-}
-
 public class TableViewMain extends Application {
 
 	@Override
-	public void start(Stage arg0) throws Exception {
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("fxml/TableView.fxml"));
+		Scene scene = new Scene(root);
 
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 
 	public static void main(String[] args) {
