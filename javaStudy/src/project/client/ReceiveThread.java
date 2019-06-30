5010674168248272bb07aa450d9c3d44330527eb
leadon <leadon@DESKTOP-R5PM2VS>
2019-07-01 04:52:12 +0900

    d

---
+++ javaStudy/src/project/client/ReceiveThread.java
@@ -0,0 +1,39 @@
+package project.client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.SocketException;
+
+public class ReceiveThread extends Thread{
+
+	private Socket m_Socket;
+	
+	@Override
+	public void run() {
+		super.run();
+		
+		try {
+			BufferedReader tmpbuf = new BufferedReader(new InputStreamReader(m_Socket.getInputStream()));
+			
+			String receiveString;
+			
+			while(true)
+			{
+				receiveString = tmpbuf.readLine();
+				
+				System.out.println("���� : " + receiveString);
+			}
+			
+		} catch (IOException e) {
+		}
+	}
+	
+	public void setSocket(Socket _socket)
+	{
+		m_Socket = _socket;
+	}
+
+}
