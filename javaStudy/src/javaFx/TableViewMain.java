c46e5997bf47517ba55d86646a8aa91cdc3ce14e
leadon <leadon@DESKTOP-R5PM2VS>
2019-06-30 16:54:43 +0900

    f

--- javaStudy/src/javaFx/TableViewMain.java
+++ javaStudy/src/javaFx/TableViewMain.java
@@ -7,32 +7,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-class Phone {
-	private SimpleStringProperty smartPhone;
-	private SimpleStringProperty image;
-
-	public Phone(String smartPhone, String image) {
-		this.smartPhone = new SimpleStringProperty(smartPhone);
-		this.image = new SimpleStringProperty(image);
-	}
-
-	public SimpleStringProperty getSmartPhone() {
-		return smartPhone;
-	}
-
-	public void setSmartPhone(SimpleStringProperty smartPhone) {
-		this.smartPhone = smartPhone;
-	}
-
-	public SimpleStringProperty getImage() {
-		return image;
-	}
-
-	public void setImage(SimpleStringProperty image) {
-		this.image = image;
-	}
-
-}
 
 public class TableViewMain extends Application {
 
