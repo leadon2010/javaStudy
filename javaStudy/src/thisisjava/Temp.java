fa1b307a5224fcb271dc371d8afea442dff7285e
leadon <leadon@DESKTOP-R5PM2VS>
2019-07-18 22:31:32 +0900

    i

--- javaStudy/src/thisisjava/Temp.java
+++ javaStudy/src/thisisjava/Temp.java
@@ -1,28 +1,43 @@
 package thisisjava;
 
-import java.io.IOException;
 
 public class Temp {
 
-	public static void main(String[] args) throws IOException {
-		int keyCode = 0;
-//		while (keyCode != 9) {
-//			if (keyCode != 13 && keyCode != 10) {
-//				System.out.println("input key");
-//				keyCode = System.in.read();
-//				System.out.println(keyCode);
-//			}
-//		}
-		int i = 0;
-		while (i < 10) {
-//			System.out.println(i);
-			int j = 0;
-			while (j < i) {
-				System.out.print("*");
-				j++;
-			}
-			System.out.println();
-			i++;
+	public static void main(String[] args) {
+
+	}
+
+	static void ary() {
+		int[] intAry = new int[5];
+		for (int i = 0; i < intAry.length; i++) {
+			System.out.println(intAry[i]);
 		}
+		intAry = new int[] { 10, 20, 30, 40, 50 };
+		System.out.println("ary length: " + intAry.length);
+		intAry = new int[] { 2, 3, 4 };
+		System.out.println("ary length: " + intAry.length);
+		System.out.println(intAry);
+
+		String[] strAry = { "hi", "my", "name", "is", "lch" };
+		for (String str : strAry) {
+			System.out.println(str);
+		}
+	}
+
+	static void gugudan() {
+		int i = 1;
+		while (i < 10) {
+			int j = 2;
+			while (j < 10) {
+				if (j >= i)
+					System.out.printf("%d * %d = %2d  ", j, i, j * i);
+				else
+					System.out.print("            ");
+				j += 2;
+			}
+			i++;
+			System.out.println();
+		}
+
 	}
 }
