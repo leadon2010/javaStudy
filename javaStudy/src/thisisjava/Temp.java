c8ccf42fc9e35d9aedabcab3185aafc7187ef318
leadon <leadon@DESKTOP-R5PM2VS>
2019-07-18 22:59:42 +0900

    d

--- javaStudy/src/thisisjava/Temp.java
+++ javaStudy/src/thisisjava/Temp.java
@@ -1,9 +1,53 @@
 package thisisjava;
 
-
 public class Temp {
 
 	public static void main(String[] args) {
+		int[] firstAry = new int[5];
+		int[] secondAry = new int[5];
+		int[] thirdAry = new int[5];
+
+		int a = 0;
+		for (int i = 0; i < firstAry.length; i++) {
+			firstAry[i] = ++a;
+		}
+		for (int i = 0; i < secondAry.length; i++) {
+			secondAry[i] = ++a;
+		}
+		for (int i = 0; i < thirdAry.length; i++) {
+			thirdAry[i] = ++a;
+		}
+
+		for (int j = 0; j < firstAry.length; j++) {
+			System.out.print(firstAry[j] + " ");
+		}
+		System.out.println();
+		for (int j = 0; j < secondAry.length; j++) {
+			System.out.print(secondAry[j] + " ");
+		}
+		System.out.println();
+		for (int j = 0; j < thirdAry.length; j++) {
+			System.out.print(thirdAry[j] + " ");
+		}
+		System.out.println();
+
+		System.out.println("=================================");
+
+		int[][] fourthAry = new int[3][5];
+		for (int i = 0; i < fourthAry.length; i++) {
+			for (int j = 0; j < fourthAry[i].length; j++) {
+				fourthAry[i][j] = ++a;
+			}
+		}
+
+		for (int i = 0; i < fourthAry.length; i++) {
+			for (int j = 0; j < fourthAry[i].length; j++) {
+				System.out.print(fourthAry[i][j] + " ");
+			}
+			System.out.println();
+		}
+
+		System.out.println("=================================");
 
 	}
 
