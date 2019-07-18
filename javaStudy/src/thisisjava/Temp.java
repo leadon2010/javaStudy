065e16cf8850c6bbd7a2efb1e77cc4eb5a7a30dd
leadon <leadon@DESKTOP-R5PM2VS>
2019-07-18 12:19:32 +0900

    j

--- javaStudy/src/thisisjava/Temp.java
+++ javaStudy/src/thisisjava/Temp.java
@@ -6,14 +6,23 @@ public class Temp {
 
 	public static void main(String[] args) throws IOException {
 		int keyCode = 0;
-		while (keyCode != 9) {
-
+//		while (keyCode != 9) {
 //			if (keyCode != 13 && keyCode != 10) {
-				System.out.println("input key");
-				keyCode = System.in.read();
-				System.out.println(keyCode);
+//				System.out.println("input key");
+//				keyCode = System.in.read();
+//				System.out.println(keyCode);
 //			}
-
+//		}
+		int i = 0;
+		while (i < 10) {
+//			System.out.println(i);
+			int j = 0;
+			while (j < i) {
+				System.out.print("*");
+				j++;
+			}
+			System.out.println();
+			i++;
 		}
 	}
 }
