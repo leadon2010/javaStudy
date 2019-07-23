2ea9b6dbda3210901ed8eb58adf0169d1ae0445c
leadon <leadon@DESKTOP-R5PM2VS>
2019-07-23 20:21:03 +0900

    jj

--- javaStudy/src/thisisjava/Temp.java
+++ javaStudy/src/thisisjava/Temp.java
@@ -1,17 +1,5 @@
 package thisisjava;
 
 public class Temp {
-	public static void main(String[] args) {
-		RamdomXy xy = new RamdomXy();
-		xy.play();
-	}
-
-	static int sum(int... arg) {
-		int result = 0;
-		for (int i = 0; i < arg.length; i++) {
-			result += arg[i];
-		}
-		return result;
-
-	}
+	
 }
