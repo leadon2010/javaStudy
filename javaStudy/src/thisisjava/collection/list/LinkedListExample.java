a83de77b9951f59784333f9e7836600dafc78257
leadon2010 <leadon@empal.com>
2019-08-02 12:07:26 +0900

    h

--- javaStudy/src/thisisjava/collection/list/LinkedListExample.java
+++ javaStudy/src/thisisjava/collection/list/LinkedListExample.java
@@ -1,5 +1,28 @@
 package thisisjava.collection.list;
 
-public class LinkedListExample {
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
+public class LinkedListExample {
+	public static void main(String[] args) {
+		List<String> list1 = new ArrayList<>();
+		List<String> list2 = new LinkedList<String>();
+
+		long startTime;
+		long endTime;
+		startTime = System.nanoTime();
+		for (int i = 0; i < 100000; i++) {
+			list1.add(0, "hello");
+		}
+		endTime = System.nanoTime();
+		System.out.println(endTime - startTime);
+
+		startTime = System.nanoTime();
+		for (int i = 0; i < 100000; i++) {
+			list2.add(0, "hello");
+		}
+		endTime = System.nanoTime();
+		System.out.println(endTime - startTime);
+	}
 }
