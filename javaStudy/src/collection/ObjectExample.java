cb5fe4ea7f34509326f9f18f5718a4697442eaeb
leadon2010 <leadon@empal.com>
2019-09-16 10:38:27 +0900

    j

--- javaStudy/src/collection/ObjectExample.java
+++
@@ -1,7 +0,0 @@
-package collection;
-
-public class ObjectExample {
-	public static void main(String[] args) {
-		System.out.println();
-	}
-}
--- javaStudy/src/thisisjava/collection/List/example/YedamReport.java
+++
@@ -1,9 +0,0 @@
-package thisisjava.collection.List.example;
-
-//	학생정보를 저장하는 프로그램(Ver.1.0)
-//	만든이: 김화정(2019.06.13)
-//	최종수정일: 2019.06.13
-
-public class YedamReport {
-
-}
