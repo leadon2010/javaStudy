cf30d0d264b9b5f29f30d68414cb10d1ae6af940
leadon2010 <leadon@empal.com>
2019-09-16 10:22:24 +0900

    hh

--- javaStudy/src/thisisjava/WhileKeyController.java
+++ javaStudy/src/thisisjava/WhileKeyController.java
@@ -12,35 +12,19 @@ public class WhileKeyController {
 				System.out.println("------------------------------");
 				System.out.println("1.증속 2.감속 3.중지");
 				System.out.println("------------------------------");
+			}
+			keyCode = System.in.read();
 
-				keyCode = System.in.read();
-				System.out.println("keyCode: " + keyCode);
-
-				if (keyCode == 49) {
-					speed++;
-					System.out.println("현재속도= " + speed);
-				} else if (keyCode == 50) {
-					speed--;
-					System.out.println("현재속도= " + speed);
-				} else if (keyCode == 51) {
-					run = false;
-				} else if (keyCode == 13) {
-					System.out.println("13: " + keyCode);
-				} else if (keyCode == 10) {
-					System.out.println("10: " + keyCode);
-				}
-
-			} else if (keyCode == 13) {
-				System.out.println("a:" + keyCode);
-				System.in.read();
-
-			} else if (keyCode == 10) {
-				System.out.println("b:" + keyCode);
-				System.in.read();
-
+			if (keyCode == 49) {
+				speed++;
+				System.out.println("현재속도= " + speed);
+			} else if (keyCode == 50) {
+				speed--;
+				System.out.println("현재속도= " + speed);
+			} else if (keyCode == 51) {
+				run = false;
 			}
 		}
-
 		System.out.println("프로그램 종료");
 	}
 }
