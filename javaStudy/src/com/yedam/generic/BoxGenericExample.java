c428d079f4afdee66a088d646c387c1ba7fa2a91
leadon2010 <leadon@empal.com>
2019-10-01 08:47:41 +0900

    f

--- javaStudy/src/com/yedam/generic/BoxGenericExample.java
+++ javaStudy/src/com/yedam/generic/BoxGenericExample.java
@@ -1,17 +1,17 @@
 package com.yedam.generic;
 
-public class GenericBox {
+public class BoxGenericExample {
 	public static void main(String[] args) {
 
 		Box<String> strBox = new Box<String>();
 		strBox.set("hello");
-		String str = strBox.get();
-		System.out.println(str);
+		String strValue = strBox.get();
+		System.out.println(strValue);
 
 		Box<Integer> intBox = new Box<Integer>();
 		intBox.set(10);
-		int value = intBox.get();
-		System.out.println(value);
+		int intValue = intBox.get();
+		System.out.println(intValue);
 
 	}
 }
