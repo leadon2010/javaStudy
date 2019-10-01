44974bb61d3c7b7d8f1fb59bffb9a0e359342086
leadon2010 <leadon@empal.com>
2019-10-01 09:22:10 +0900

    kljk

---
+++ javaStudy/src/com/yedam/generic/ProductsExample.java
@@ -0,0 +1,11 @@
+package com.yedam.generic;
+
+public class ProductsExample {
+	public static void main(String[] args) {
+		Products<Tv, String, String> products = new Products<>();
+		Tv tv = products.getKind();
+		String model = products.getModel();
+		String value = products.getValue();
+		System.out.printf("%2s%2s%2s", tv, model, value);
+	}
+}
