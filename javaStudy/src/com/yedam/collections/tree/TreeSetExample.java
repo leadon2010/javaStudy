17f78df48a60bf1f111f2c5d0b5d23ecab7479b7
lch <leadon@empal.com>
2019-10-02 12:14:10 +0900

    j

---
+++ javaStudy/src/com/yedam/collections/tree/TreeSetExample.java
@@ -0,0 +1,15 @@
+package com.yedam.collections.tree;
+
+import java.util.TreeSet;
+
+public class TreeSetExample {
+	public static void main(String[] args) {
+		TreeSet<Integer> scores = new TreeSet<>();
+		scores.add(new Integer(87));
+		scores.add(new Integer(98));
+		scores.add(new Integer(75));
+		scores.add(new Integer(95));
+		scores.add(new Integer(80));
+		
+	}
+}
