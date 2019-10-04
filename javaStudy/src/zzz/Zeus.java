f8609464a1c1006c103126a5c3b7d17384d1e7e2
leadon2010 <leadon@empal.com>
2019-10-04 09:14:34 +0900

    ㅏ

--- javaStudy/src/zzz/Zeus.java
+++ javaStudy/src/zzz/Zeus.java
@@ -1,7 +1,43 @@
 package zzz;
 
+class Student {
+	String name;
+	String studentNo;
+	String major;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getStudentNo() {
+		return studentNo;
+	}
+
+	public void setStudentNo(String studentNo) {
+		this.studentNo = studentNo;
+	}
+
+	public String getMajor() {
+		return major;
+	}
+
+	public void setMajor(String major) {
+		this.major = major;
+	}
+
+}
+
 public class Zeus {
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		execute();
+	}
+	public static void execute() {
+		System.out.println("=============================");
+		System.out.println("");
+		System.out.println("=============================");
 	}
 }
