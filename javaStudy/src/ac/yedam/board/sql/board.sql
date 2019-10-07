14de3a709608e321bea3ebfd4542f1d0ad4bc4c3
leadon2010 <leadon@empal.com>
2019-10-07 11:29:57 +0900

    jj

--- javaStudy/src/ac/yedam/board/sql/board.sql
+++ javaStudy/src/ac/yedam/board/sql/board.sql
@@ -10,7 +10,7 @@ id varchar2(10), passwd varchar2(10), name varchar2(100));
 insert into login_test values('1111','1234','changho');
 insert into login_test values('166','1234','Susan');
 
---°Ô½ÃÆÇ
+--ê²Œì‹œíŒ
 CREATE TABLE board(board_no NUMBER NOT NULL
                   ,title VARCHAR2(100)
                   ,content VARCHAR2(3000)
