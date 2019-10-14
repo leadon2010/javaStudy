cbc4eaf65d4a879a052a344d16a3e0954cf4fa90
leadon2010 <leadon@empal.com>
2019-10-14 11:00:02 +0900

    h

--- javaStudy/src/lambda/thisisjava/PredicateAndOrNegateExample.java
+++ javaStudy/src/lambda/thisisjava/PredicateAndOrNegateExample.java
@@ -4,8 +4,10 @@ import java.util.function.IntPredicate;
 
 public class PredicateAndOrNegateExample {
 	public static void main(String[] args) {
-		IntPredicate predicateA = a -> a % 2 == 0;
-		IntPredicate predicateB = (a) -> a % 3 == 0;
+		IntPredicate predicateA = (a) -> {
+			return a % 2 == 0;
+		};
+		IntPredicate predicateB = a -> a % 3 == 0;
 		IntPredicate predicateAB;
 		boolean result;
 
@@ -16,11 +18,10 @@ public class PredicateAndOrNegateExample {
 		predicateAB = predicateA.or(predicateB);
 		result = predicateAB.test(9);
 		System.out.println("9는 2또는 3의 배수입니까? " + result);
-		
+
 		predicateAB = predicateA.negate();
 		result = predicateAB.test(9);
 		System.out.println("9는 홀수입니까? " + result);
-		
 
 	}
 }
