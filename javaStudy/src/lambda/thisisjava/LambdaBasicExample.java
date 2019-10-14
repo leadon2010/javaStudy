40ad2167c3fa303983a0cb462fc6d2a81529d944
lch <leadon@empal.com>
2019-10-15 06:14:36 +0900

    f

--- javaStudy/src/lambda/thisisjava/LambdaBasicExample.java
+++ javaStudy/src/lambda/thisisjava/LambdaBasicExample.java
@@ -8,27 +8,27 @@ interface MyInterface {
 public class LambdaBasicExample {
 	public static void main(String[] args) {
 
+		Runnable runnable = new Runnable() {
+
+			@Override
+			public void run() {
+				System.out.println("Runnable express.");
+			}
+
+		};
+		runnable.run();
+
+		runnable = () -> {
+			System.out.println("Lambda Expression");
+		};
+		runnable.run();
+
 		MyInterface myInterface = (num1, num2) -> {
 			return num1 + num2;
 		};
 
 		int result = myInterface.print(3, 5);
 		System.out.println("result is " + result);
-
-//		Runnable runnable = new Runnable() {
-//
-//			@Override
-//			public void run() {
-//				System.out.println("Runnable express.");
-//			}
-//
-//		};
-//		runnable.run();
-//
-//		runnable = () -> {
-//			System.out.println("Lambda Expression");
-//		};
-//		runnable.run();
 
 	} // end of main
 } // end of class
