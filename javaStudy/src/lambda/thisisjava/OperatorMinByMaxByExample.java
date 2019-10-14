561fe1074159607f78f3c0d9dd36612b6f1aff7f
leadon2010 <leadon@empal.com>
2019-10-14 11:11:29 +0900

    h

--- javaStudy/src/lambda/thisisjava/OperatorMinByMaxByExample.java
+++ javaStudy/src/lambda/thisisjava/OperatorMinByMaxByExample.java
@@ -19,7 +19,9 @@ public class OperatorMinByMaxByExample {
 		BinaryOperator<Fruit> binaryOperator;
 		Fruit fruit;
 
-		binaryOperator = BinaryOperator.minBy((f1, f2) -> Integer.compare(f1.price, f2.price));
+		binaryOperator = BinaryOperator.minBy((f1, f2) -> {
+			return Integer.compare(f1.price, f2.price);
+		});
 		fruit = binaryOperator.apply(new Fruit("딸기", 6000), new Fruit("수박", 10000));
 		System.out.println("minBy: " + fruit.name);
 
