bac7b847ef9a7cf685ae3089dc84f69171d044f6
leadon2010 <leadon@empal.com>
2019-10-14 09:38:43 +0900

    k

--- javaStudy/src/lambda/RunnableExample.java
+++ javaStudy/src/lambda/RunnableExample.java
@@ -4,13 +4,29 @@ public class RunnableExample {
 	public static void main(String[] args) {
 		Runnable runnable = () -> {
 			for (int i = 0; i < 10; i++) {
-				System.out.println(i);
+				System.out.println("a=>" + i);
+				try {
+					Thread.sleep(500);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
 			}
 		};
 		System.out.println("===========================");
 		Thread thread = new Thread(runnable);
 		thread.start();
-		Thread thread1 = new Thread(runnable);
-		thread1.start();
+
+		thread = new Thread(() -> {
+			for (int i = 0; i < 10; i++) {
+				System.out.println("b=>" + i);
+				try {
+					Thread.sleep(500);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		thread.start();
+		System.out.println("===========================");
 	}
 }
