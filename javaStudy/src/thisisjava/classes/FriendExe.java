b8b92e346c07a43713c099a5cc27089867eb3306
leadon <leadon@localhost>
2020-02-03 22:47:35 +0900

    ff

--- javaStudy/src/thisisjava/classes/FriendExe.java
+++ javaStudy/src/thisisjava/classes/FriendExe.java
@@ -33,16 +33,48 @@ public class FriendExe {
 		}
 	}// end of main
 
+	static void saveFriend(Friends friend) {
+		for (int i = 0; i < fArray.length; i++) {
+			if (fArray[i] == null) {
+				fArray[i] = friend;
+				break;
+			}
+		}
+	}
+
 	static void comFriend() {
 		System.out.println("회사명을 입력");
+		String companyName = sc.nextLine();
+		System.out.println("부서명을 입력:");
+		String deptName = sc.nextLine();
+		System.out.println("이름입력:");
+		String name = sc.nextLine();
+		System.out.println("전화번호입력:");
+		String phone = sc.nextLine();
+		ComFriends friend = new ComFriends(name, phone, companyName, deptName);
+		saveFriend(friend);
 	}
 
 	static void univFriend() {
-
+		System.out.println("학교명을 입력");
+		String companyName = sc.nextLine();
+		System.out.println("전공을 입력:");
+		String deptName = sc.nextLine();
+		System.out.println("이름입력:");
+		String name = sc.nextLine();
+		System.out.println("전화번호입력:");
+		String phone = sc.nextLine();
+		UnivFriends friend = new UnivFriends(name, phone, companyName, deptName);
+		saveFriend(friend);
 	}
 
 	static void etcFriend() {
-
+		System.out.println("이름입력:");
+		String name = sc.nextLine();
+		System.out.println("전화번호입력:");
+		String phone = sc.nextLine();
+		Friends friend = new Friends(name, phone);
+		saveFriend(friend);
 	}
 
 	static void addFriend() {
@@ -62,7 +94,13 @@ public class FriendExe {
 	}
 
 	static void findFriend() {
-
+		System.out.println("조회할 이름입력:");
+		String name = sc.nextLine();
+		for (Friends friend : fArray) {
+			if (friend != null && friend.getName().equals(name)) {
+				System.out.println(friend);
+			}
+		}
 	}
 
 	static void listFriend() {
