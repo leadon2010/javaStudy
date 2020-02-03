5c87c6c1505dedc0afd8dfb01d5f9a51737b6aee
leadon2010 <leadon@empal.com>
2020-02-03 20:35:49 +0900

    d

--- javaStudy/src/chap5_class/EarthExample.java
+++
@@ -1,8 +0,0 @@
-package chap5_class;
-
-public class EarthExample {
-	public static void main(String[] args) {
-		System.out.println("지구 반지름: " + Earth.EARTH_RADIUS);
-		System.out.println("지구 표면적: " + Earth.EARTH_SURFACE_AREA);
-	}
-}
