c50afbfa31e883cc4d667c172289fedaf6d6b5ec
leadon <leadon@localhost>
2020-04-10 06:54:32 +0900

    fd

--- javaStudy/src/javafx/AppMain.java
+++ javaStudy/src/javafx/AppMain.java
@@ -10,7 +10,7 @@ public class AppMain extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("Label.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("Binding.fxml"));
 		Scene scene = new Scene(root);
 		
 		primaryStage.setTitle("AppMain");
