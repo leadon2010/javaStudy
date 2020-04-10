757e7e63565e49f5daf6e72c444e0788c564d257
leadon <leadon@localhost>
2020-04-10 19:47:03 +0900

    dfd

--- javaStudy/src/javafx/AppMain.java
+++ javaStudy/src/javafx/AppMain.java
@@ -10,7 +10,7 @@ public class AppMain extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("TilePane.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("GridPane.fxml"));
 		Scene scene = new Scene(root);
 		
 		primaryStage.setTitle("AppMain");
