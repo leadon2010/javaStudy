f1cda7b4a8c6cdd62f24d958a63b22d0737bd4e6
winny <winny@winny-desktop>
2020-04-17 16:18:48 +0900

    dfd

--- javaStudy/src/project/fileio/CellPhone.java
+++ javaStudy/src/project/fileio/CellPhone.java
@@ -7,6 +7,8 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class CellPhone {
@@ -22,37 +24,43 @@ public class CellPhone {
 			this.age = age;
 			this.phone = phone;
 		}
+
 	} // end of Address
 
 	static Scanner scn = new Scanner(System.in);
 
 	public static void main(String[] args) {
 //		1. view_juso() 2.add_juso() 3.del_juso() 4.exit
-		
+
 		int menu;
-		while(true) {
+		while (true) {
 			System.out.println("============================");
 			System.out.println("1.view 2.add 3.del 4.exit");
 			System.out.println("============================");
-			
-			menu = scn.nextInt();scn.nextLine();
-			
+
+			menu = scn.nextInt();
+			scn.nextLine();
+
 			if (menu == 1) {
 				view_juso();
+
 			} else if (menu == 2) {
 				add_juso();
+
 			} else if (menu == 3) {
-				
+				del_juso();
+
 			} else if (menu == 4) {
 				break;
 			}
 		} // end of while
-		
+
 		System.out.println("==== end of program ====");
+
 	} // end of main()
-	
+
 	public static void view_juso() {
-		
+
 		File file = new File(fileName);
 		if (!file.exists()) {
 			try {
@@ -65,13 +73,12 @@ public class CellPhone {
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(fileName));
 			int rowCnt = 0;
-			while(true) {
+			while (true) {
 				if (!br.ready())
 					break;
-				
+
 				String str = br.readLine();
-				System.out.println(str);
-				rowCnt++;
+				System.out.printf("%2d %s \n", ++rowCnt, str);
 			}
 			if (rowCnt == 0) {
 				System.out.println("no line.");
@@ -82,8 +89,9 @@ public class CellPhone {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	}
-	
+
+	} // end of view_juso()
+
 	public static void add_juso() {
 		try {
 			BufferedWriter bw = new BufferedWriter(new FileWriter(fileName, true));
@@ -93,21 +101,61 @@ public class CellPhone {
 			String age = scn.nextLine();
 			System.out.print("phone>> ");
 			String phone = scn.nextLine();
-			
+
 			Address addr = new Address(name, age, phone);
 			bw.write(addr.name + " " + addr.age + " " + addr.phone);
 			bw.newLine();
 			bw.flush();
 			bw.close();
 			System.out.println("writed.");
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	}
-	
-	public void del_juso() {
-		
-	}
+
+	} // end of add_juso()
+
+	public static void del_juso() {
+		List<String> temp = new ArrayList<>();
+		String str = "";
+		BufferedReader br;
+		System.out.print("choose del line number: ");
+		int delLine = scn.nextInt();
+		try {
+			br = new BufferedReader(new FileReader(fileName));
+
+			for (int i = 1;; i++) {
+
+				if ((str = br.readLine()) == null)
+					break;
+				else {
+					if (i != delLine) {
+						temp.add(str);
+					} else {
+						System.out.println(i + " line deleted.");
+					}
+				}
+			}
+			br.close();
+
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		try {
+			BufferedWriter bw = new BufferedWriter(new FileWriter(fileName));
+			for (String strs : temp) {
+				bw.write(strs);
+				bw.newLine();
+			}
+			bw.flush();
+			bw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	} // end of del_juso()
 
 } // end of class
