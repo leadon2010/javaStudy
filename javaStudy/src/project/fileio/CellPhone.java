927120ac9b6450ee46aebce5fb6982ba308b9d74
leadon <leadon@localhost>
2020-04-17 14:04:49 +0900

    dfd

--- javaStudy/src/project/fileio/CellPhone.java
+++ javaStudy/src/project/fileio/CellPhone.java
@@ -1,5 +1,10 @@
 package project.fileio;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
@@ -22,23 +27,87 @@ public class CellPhone {
 	static Scanner scn = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		System.out.println("name");
-		String name = scn.nextLine();
-		System.out.println("age");
-		String age = scn.nextLine();
-		System.out.println("phone");
-		String phone = scn.nextLine();
-
-		Address addr = new Address(name, age, phone);
-
+//		1. view_juso() 2.add_juso() 3.del_juso() 4.exit
+		
+		int menu;
+		while(true) {
+			System.out.println("============================");
+			System.out.println("1.view 2.add 3.del 4.exit");
+			System.out.println("============================");
+			
+			menu = scn.nextInt();scn.nextLine();
+			
+			if (menu == 1) {
+				view_juso();
+			} else if (menu == 2) {
+				add_juso();
+			} else if (menu == 3) {
+				
+			} else if (menu == 4) {
+				break;
+			}
+		} // end of while
+		
+		System.out.println("==== end of program ====");
+	} // end of main()
+	
+	public static void view_juso() {
+		
+		File file = new File(fileName);
+		if (!file.exists()) {
+			try {
+				BufferedWriter bw = new BufferedWriter(new FileWriter(fileName));
+				bw.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 		try {
-			FileWriter fw = new FileWriter(fileName);
-			fw.write(addr.name + ", " + addr.age + ", " + addr.phone);
-			fw.flush();
-			fw.close();
-
+			BufferedReader br = new BufferedReader(new FileReader(fileName));
+			int rowCnt = 0;
+			while(true) {
+				if (!br.ready())
+					break;
+				
+				String str = br.readLine();
+				System.out.println(str);
+				rowCnt++;
+			}
+			if (rowCnt == 0) {
+				System.out.println("no line.");
+			}
+			br.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-}
+	
+	public static void add_juso() {
+		try {
+			BufferedWriter bw = new BufferedWriter(new FileWriter(fileName, true));
+			System.out.print("name>> ");
+			String name = scn.nextLine();
+			System.out.print("age>> ");
+			String age = scn.nextLine();
+			System.out.print("phone>> ");
+			String phone = scn.nextLine();
+			
+			Address addr = new Address(name, age, phone);
+			bw.write(addr.name + " " + addr.age + " " + addr.phone);
+			bw.newLine();
+			bw.flush();
+			bw.close();
+			System.out.println("writed.");
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void del_juso() {
+		
+	}
+
+} // end of class
