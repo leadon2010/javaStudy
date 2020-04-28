482675017a3ba2838e2fbeb94bc82c70cb63944a
leadon <leadon@localhost>
2020-04-28 11:29:21 +0900

    dfd

--- javaStudy/src/basic/ForExample.java
+++ javaStudy/src/basic/ForExample.java
@@ -2,7 +2,11 @@ package basic;
 
 public class ForExample {
 	public static void main(String[] args) {
-
+		int sum = 0;
+		for (int i = 1; i <= 100; i++) {
+			sum += i;
+		}
+		System.out.println("sum: " + sum);
 	}
 
 }
