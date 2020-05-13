2ea2e11cabf445893eff54b7c0290335e85d2914
leadon <leadon@fedora31>
2020-05-13 22:54:52 +0900

    dfd

---
+++ javaStudy/src/classes/RemoteControlExample.java
@@ -0,0 +1,30 @@
+package classes;
+
+public class RemoteControlExample {
+	public static void main(String[] args) {
+		RemoteControl rc = new RemoteControl() {
+			int volume;
+			
+			@Override
+			public void turnOn() {
+				System.out.println("익명객체 전원 켬.");
+			}
+
+			@Override
+			public void turnOff() {
+				System.out.println("익명객체 전원 끔.");
+			}
+
+			@Override
+			public void setVolume(int volume) {
+				this.volume = volume;
+				System.out.println("볼륨을 " + volume + " 으로 변경.");
+			}
+			
+		};
+		
+		rc.turnOn();
+		rc.setVolume(10);
+		rc.turnOff();
+	}
+}
---
+++ javaStudy/src/classes/SmartTelevision.java
@@ -0,0 +1,25 @@
+package classes;
+
+public class SmartTelevision implements RemoteControl, Searchable {
+	int volume;
+
+	@Override
+	public void search(String url) {
+		System.out.println(url + " 을 검색합니다.");
+	}
+
+	@Override
+	public void turnOn() {
+		System.out.println("SmartTV 전원을 켭니다.");
+	}
+
+	@Override
+	public void turnOff() {
+		System.out.println("SmartTV 전원을 끕니다.");
+	}
+
+	@Override
+	public void setVolume(int volume) {
+		System.out.println("SmartTV " + volume + " 변경.");
+	}
+}
