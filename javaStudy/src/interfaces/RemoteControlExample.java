5995657f17e02b9d8aafc0a6b9f8159a69f34896
leadon <leadon@fedora31>
2020-05-14 08:48:09 +0900

    dfsa

--- javaStudy/src/interfaces/RemoteControlExample.java
+++ javaStudy/src/interfaces/RemoteControlExample.java
@@ -4,7 +4,7 @@ public class RemoteControlExample {
 	public static void main(String[] args) {
 		RemoteControl rc = new RemoteControl() {
 			int volume;
-			
+
 			@Override
 			public void turnOn() {
 				System.out.println("익명객체 전원 켬.");
@@ -20,9 +20,9 @@ public class RemoteControlExample {
 				this.volume = volume;
 				System.out.println("볼륨을 " + volume + " 으로 변경.");
 			}
-			
+
 		};
-		
+
 		rc.turnOn();
 		rc.setVolume(10);
 		rc.turnOff();
