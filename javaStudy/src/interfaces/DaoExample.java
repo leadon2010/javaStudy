5f75c469ca756063c819da5381af395f2cc92878
leadon <leadon@fedi>
2020-05-19 06:47:21 +0900

    df

---
+++ javaStudy/src/interfaces/DaoExample.java
@@ -0,0 +1,55 @@
+package interfaces;
+
+class OracleDao implements DataAccessObject {
+	String db = "Oracle DB";
+	@Override
+	public void select() {
+		System.out.println(db + "에서 검색");
+	}
+	@Override
+	public void insert() {
+		System.out.println(db + "에 삽입");
+	}
+	@Override
+	public void update() {
+		System.out.println(db + "를 수정");
+	}
+	@Override
+	public void delete() {
+		System.out.println(db + "에서 삭제");
+	}
+}
+
+class MySqlDao implements DataAccessObject {
+	String db = "MySql DB";
+	@Override
+	public void select() {
+		System.out.println(db + "에서 검색");
+	}
+	@Override
+	public void insert() {
+		System.out.println(db + "에 삽입");
+	}
+	@Override
+	public void update() {
+		System.out.println(db + "를 수정");
+	}
+	@Override
+	public void delete() {
+		System.out.println(db + "에서 삭제");
+	}
+}
+
+public class DaoExample {
+	public static void dbWork(DataAccessObject dao) {
+		dao.select();
+		dao.insert();
+		dao.update();
+		dao.delete();
+	}
+
+	public static void main(String[] args) {
+		dbWork(new OracleDao());
+		dbWork(new MySqlDao());
+	}
+}
