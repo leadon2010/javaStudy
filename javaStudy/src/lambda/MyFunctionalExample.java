394eaf3c1da78c0dc07e520863b26e678a75f0e6
leadon <leadon@fedi>
2020-05-21 19:39:48 +0900

    f

---
+++ javaStudy/src/lambda/MyFunctionalExample.java
@@ -0,0 +1,13 @@
+package lambda;
+
+@FunctionalInterface
+interface MyFunctionalInterface {
+	public void run();
+}
+
+public class MyFunctionalExample {
+
+	public static void main(String[] args) {
+
+	} // end of main
+}
