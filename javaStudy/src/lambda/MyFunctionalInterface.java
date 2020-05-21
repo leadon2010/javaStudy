cd19b2754ef707ed3721c16e4287b9e6286eee2a
leadon <leadon@fedi>
2020-05-21 19:20:21 +0900

    dfd

--- javaStudy/src/lambda/MyFunctionalInterface.java
+++ javaStudy/src/lambda/MyFunctionalInterface.java
@@ -2,6 +2,11 @@ package lambda;
 
 @FunctionalInterface
 public interface MyFunctionalInterface {
-	public String run(String str);
+	public String run(String str, String str2);
 //	public void method2();
+}
+
+@FunctionalInterface
+interface SummariesInterface {
+	public int sum(int x, int y);
 }
