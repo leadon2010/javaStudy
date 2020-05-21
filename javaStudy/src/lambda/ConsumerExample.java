e8c41ca47c1a4ac8dfc3f3c44f2bcfe501a05411
winnie <winnie@DESKTOP-TE98KVK>
2020-05-21 21:49:25 +0900

    dfd

--- javaStudy/src/lambda/ConsumerExample.java
+++ javaStudy/src/lambda/ConsumerExample.java
@@ -1,9 +1,12 @@
 package lambda;
 
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class ConsumerExample {
 	public static void main(String[] args) {
 		Consumer<String> consumer = null;
+		
+		Function<String,String> function;
 	}
 }
