d8bcdf3218b0ca660f6c9ab359c65ac92a3e6d1a
leadon <leadon@fedi>
2020-05-21 20:19:51 +0900

    fd

--- javaStudy/src/lambda/MyFunctionalExample.java
+++ javaStudy/src/lambda/MyFunctionalExample.java
@@ -5,9 +5,24 @@ interface MyFunctionalInterface {
 	public void run();
 }
 
+class UsingLocal {
+	void method(int arg) {
+		int localVar = 40;
+//		arg = 30;
+//		localVar = 30;
+		MyFunctionalInterface fi = () -> {
+			System.out.println("arg : " + arg);
+			System.out.println("localVar : " + localVar);
+		};
+		fi.run();
+	}
+}
+
 public class MyFunctionalExample {
 
 	public static void main(String[] args) {
+		UsingLocal ul = new UsingLocal();
+		ul.method(30);
 
 	} // end of main
 }
