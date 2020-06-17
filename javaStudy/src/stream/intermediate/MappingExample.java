0263cc3bb8f4a2d55645e91bc0a068ba3a8ddc30
winnie <winnie@DESKTOP-KNFHCOA>
2020-06-17 18:37:28 +0900

    f

--- javaStudy/src/stream/intermediate/MappingExample.java
+++ javaStudy/src/stream/intermediate/MappingExample.java
@@ -1,110 +1,55 @@
 package stream.intermediate;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.Arrays;
-import java.util.List;
 import java.util.function.Function;
+import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class MappingExample {
 	public static void main(String[] args) {
 //		IntStream.rangeClosed(1, 5).map(opr -> opr * 2).forEach(System.out::println);
-		Employee.persons().stream().map(new Function<Employee, String>() {
 
+		// map example
+		Employee.persons().stream().map(new Function<Employee, String>() {
 			@Override
 			public String apply(Employee t) {
 				return t.toString();
 			}
-
 		}).forEach(System.out::println);
+		System.out.println("---------------------------------------");
 
+		Employee.persons().stream().flatMapToDouble(new Function<Employee, DoubleStream>() {
+			@Override
+			public DoubleStream apply(Employee t) {
+				return DoubleStream.of(t.getIncome());
+			}
+		}).forEach(System.out::println);
+		System.out.println("---------------------------------------");
+
+		// flatMap example
+		Stream.of(1, 2, 3).flatMap(new Function<Integer, Stream<Integer>>() {
+			@Override
+			public Stream<Integer> apply(Integer t) {
+				return Stream.of(t, t * 2);
+			}
+		}).forEach(System.out::println);
+		System.out.println("---------------------------------------");
+
+		Stream.of(1, 3, 5).flatMapToInt(new Function<Integer, IntStream>() {
+			@Override
+			public IntStream apply(Integer t) {
+				return IntStream.of(t, t + 1, t * 10);
+			}
+		}).forEach(s -> System.out.print(s + "\t"));
+		System.out.println();
+		System.out.println("---------------------------------------");
+
+		double sum = Stream.of(1, 5, 9).flatMapToDouble(new Function<Integer, DoubleStream>() {
+			@Override
+			public DoubleStream apply(Integer t) {
+				return DoubleStream.of(t);
+			}
+		}).sum();
+		System.out.println(sum);
 	}
-}
-
-class Employee {
-	public static enum Gender {
-		MALE, FEMALE;
-	}
-
-	private long id;
-	private String name;
-	private Gender gender;
-	private LocalDate dateOfBirth;
-	private double income;
-
-	public Employee(long id, String name, Gender gender, LocalDate dateOfBirth, double income) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.gender = gender;
-		this.dateOfBirth = dateOfBirth;
-		this.income = income;
-	}
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Gender getGender() {
-		return gender;
-	}
-
-	public void setGender(Gender gender) {
-		this.gender = gender;
-	}
-
-	public LocalDate getDateOfBirth() {
-		return dateOfBirth;
-	}
-
-	public void setDateOfBirth(LocalDate dateOfBirth) {
-		this.dateOfBirth = dateOfBirth;
-	}
-
-	public double getIncome() {
-		return income;
-	}
-
-	public void setIncome(double income) {
-		this.income = income;
-	}
-
-	public boolean isMale() {
-		return this.gender == Gender.MALE;
-	}
-
-	public boolean isFemale() {
-		return this.gender == Gender.FEMALE;
-	}
-
-	@Override
-	public String toString() {
-		String str = String.format("(%s, %s, %s, %s, %.2f)", id, name, gender, dateOfBirth, income);
-		return str;
-	}
-
-	public static List<Employee> persons() {
-		Employee p1 = new Employee(1, "Hong", Gender.MALE, LocalDate.of(1991, Month.JANUARY, 1), 2343.0);
-		Employee p2 = new Employee(2, "Hwang", Gender.MALE, LocalDate.of(1992, Month.JULY, 21), 7100.0);
-		Employee p3 = new Employee(3, "Choi", Gender.FEMALE, LocalDate.of(1993, Month.MAY, 29), 5455.0);
-		Employee p4 = new Employee(4, "Park", Gender.MALE, LocalDate.of(1994, Month.OCTOBER, 16), 1800.0);
-		Employee p5 = new Employee(5, "Kim", Gender.FEMALE, LocalDate.of(1995, Month.DECEMBER, 13), 1234.0);
-		Employee p6 = new Employee(6, "Ryu", Gender.MALE, LocalDate.of(1996, Month.JUNE, 9), 3211.0);
-		List<Employee> persons = Arrays.asList(p1, p2, p3, p4, p5);
-		return persons;
-	}
-
 }
