22c57d13c65e0abbca109b0ad1a0beb549fbc86a
leadon <leadon@leadon-B55PQ-AS5G21D>
2020-06-20 10:21:00 +0900

    f

---
+++ javaStudy/src/streams/collections/CollectExample2.java
@@ -0,0 +1,35 @@
+package streams.collections;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Supplier;
+
+public class CollectExample2 {
+	public static void main(String[] args) {
+		List<Student> totalist = Arrays.asList(new Student("Hong1", 23, Student.Sex.MALE),
+				new Student("Hong2", 23, Student.Sex.FEMALE), new Student("Hong3", 23, Student.Sex.MALE),
+				new Student("Hong4", 23, Student.Sex.FEMALE));
+
+		MaleStudent maleStudent = totalist.parallelStream().filter(s -> s.getSex() == Student.Sex.MALE)
+				.collect(new Supplier<MaleStudent>() {
+					@Override
+					public MaleStudent get() {
+						return new MaleStudent();
+					}
+				}, new BiConsumer<MaleStudent, Student>() {
+					@Override
+					public void accept(MaleStudent t, Student u) {
+						t.accumulate(u);
+					}
+				}, new BiConsumer<MaleStudent, MaleStudent>() {
+					@Override
+					public void accept(MaleStudent t, MaleStudent u) {
+						t.combine(u);
+					}
+				});
+
+		maleStudent.getList().stream().forEach(s -> System.out.println(s.getName()));
+
+	}
+}
