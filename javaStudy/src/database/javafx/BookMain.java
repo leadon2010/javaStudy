a1f4f3378bec0dcebbfc932b6c9bf82c880b93fe
cholee <cholee@hrd.net>
2020-09-08 16:53:44 +0900

    f

--- javaStudy/src/database/javafx/BookMain.java
+++ javaStudy/src/database/javafx/BookMain.java
@@ -1,13 +1,25 @@
 package database.javafx;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class BookMain extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("BookMain.fxml"));
+		Parent root = loader.load();
+
+		BookController controller = loader.getController();
+		controller.setPrimaryStage(primaryStage);
+
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+
 	}
 
 	public static void main(String[] args) {
--- javaStudy/src/streams/StreamExample3.java
+++ javaStudy/src/streams/StreamExample3.java
@@ -2,36 +2,51 @@ package streams;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.ToIntFunction;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class StreamExample3 {
+	static int sum = 0, cnt = 0;
+
 	public static void main(String[] args) {
 		List<Student> list = Arrays.asList(new Student("Hong", 90), new Student("Park", 88), new Student("Choi", 80),
 				new Student("Kwon", 87));
 
-		Stream<Student> streams = list.stream();
-		Double result = streams.mapToInt(new ToIntFunction<Student>() {
+		Stream<Student> students = list.stream();
+
+		students.forEach(new Consumer<Student>() {
 			@Override
-			public int applyAsInt(Student value) {
-				System.out.println(value.name + ", " + value.score);
-				return value.score;
+			public void accept(Student t) {
+				sum += t.score;
+				cnt++;
+
 			}
+		});
+		System.out.println(sum / (double) cnt);
 
-		}).average().getAsDouble();
-		System.out.println(" 결과: " + result);
-
-		streams = list.stream();
-		result = streams.flatMapToInt(new Function<Student, IntStream>() {
-			@Override
-			public IntStream apply(Student t) {
-				System.out.println(t.name + ", " + t.score);
-				return IntStream.of(t.score);
-			}
-
-		}).average().getAsDouble();
-		System.out.println(" 결과: " + result);
+//		Stream<Student> streams = list.stream();
+//		Double result = streams.mapToInt(new ToIntFunction<Student>() {
+//			@Override
+//			public int applyAsInt(Student value) {
+//				System.out.println(value.name + ", " + value.score);
+//				return value.score;
+//			}
+//
+//		}).average().getAsDouble();
+//		System.out.println(" 결과: " + result);
+//
+//		streams = list.stream();
+//		result = streams.flatMapToInt(new Function<Student, IntStream>() {
+//			@Override
+//			public IntStream apply(Student t) {
+//				System.out.println(t.name + ", " + t.score);
+//				return IntStream.of(t.score);
+//			}
+//
+//		}).average().getAsDouble();
+//		System.out.println(" 결과: " + result);
 	}
 }
