0d18fa5055e495388610ba6500ca240d3b69f592
cholee <cholee@hrd.net>
2020-12-07 08:39:55 +0900

    h

--- javaStudy/src/api/Temp.java
+++ javaStudy/src/api/Temp.java
@@ -1,35 +1,8 @@
 package api;
 
-import java.util.Calendar;
-
 public class Temp {
 	public static void main(String[] args) {
-		Calendar cal = Calendar.getInstance();
-		System.out.println(cal.get(Calendar.DAY_OF_WEEK));
-		System.out.println(cal.getActualMaximum(Calendar.DATE));
-
-		cal.set(2020, 11, 13);
-		System.out.println(cal.get(Calendar.DAY_OF_WEEK));
-		System.out.println(cal.getActualMaximum(Calendar.DATE));
-		int temp = 2;
-		if (temp % 2 == 1) {
-			System.out.print(String.format("%3d", 12));
-			System.out.print(String.format("%3d", 12));
-			System.out.print(String.format("%3d", 12));
-			System.out.print(String.format("%3d", 12));
-			System.out.println();
-		}
-		System.out.print(String.format("%4d", 12));
-		System.out.print(String.format("%4d", 12));
-		System.out.print(String.format("%4d", 12));
-		System.out.print(String.format("%4d", 12));
-		System.out.println();
-		System.out.print(String.format("%5d", 12));
-		System.out.print(String.format("%5d", 12));
-		System.out.print(String.format("%5d", 12));
-		System.out.print(String.format("%5d", 12));
-		System.out.println();
-
-		System.out.printf("%3d", 5);
+		String str = "Hello";
+		System.out.println(str.hashCode());
 	}
 }
