b906644972e021fc39381deb841471e36b74304f
cholee <cholee@hrd.net>
2021-03-05 17:43:06 +0900

    f

--- javaStudy/src/classes/abstract_class/AnimalExample.java
+++ javaStudy/src/classes/abstract_class/AnimalExample.java
@@ -9,9 +9,15 @@ public class AnimalExample {
 		animal.showInfo();
 
 		Cat cat = new Cat("포유류");
+		cat = new Cat("포유류");
 		cat.breathe();
 		cat.sound();
 		cat.showInfo();
+		System.out.println("----------------------");
+
+		// 변수의 다형성을 ...
+		animalSound(animal);
+		animalSound(cat);
 
 		// final 변수 일 경우에도 값을 변경할 수 없다.
 		final int age = 10;
@@ -20,8 +26,13 @@ public class AnimalExample {
 		}
 
 	}
+
+	public static void animalSound(Animal animal) {
+		animal.sound();
+	}
 }
 
+// final 인 경우에는 상속이 안된다.
 final class LastClass {
 	void run() {
 		System.out.println("last class");
